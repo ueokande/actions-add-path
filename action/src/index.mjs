6a677a6e02be37be022c77a42f7fc5aa6b93ffea
Shin'ya Ueoka <ueokande@i-beam.org>
2024-03-02 09:38:51 +0000

    Add test action

---
+++ action/src/index.mjs
@@ -0,0 +1,26 @@
+import * as core from "@actions/core";
+import * as os from "node:os";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const content = `#!/bin/sh
+echo Hello, world!
+`
+
+async function run() {
+  try {
+    const root = await fs.promises.mkdtemp(path.join(os.tmpdir(), "action-"));
+    const bin = path.join(root, "my-bin");
+    await fs.promises.writeFile(bin, content, { mode: 0o755 })
+
+    core.info("Created at", bin);
+
+    core.addPath(bin)
+
+  } catch (error) {
+    console.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
